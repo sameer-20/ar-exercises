cc91bae819ec18a2e53721f4e9a53bb8d3644d06
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-18 18:36:12 +0000

    Added validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,12 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Ask the user for a store name (store it in a variable)
+puts "Please enter a store name: "
+user_response = gets.chomp
+
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+add_store = Store.create(name: "#{user_response}")
+
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+add_store.errors.messages.each { |x| puts x }
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,19 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  # Add validations
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # Custom validation: Stores must carry at least one of the men's or women's apparel
+  validate :gender_apparel
+
+  private
+
+  def gender_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:error_msg, "Should carry at least one of the men's or women's apparel")
+    end
+  end
+
 end
