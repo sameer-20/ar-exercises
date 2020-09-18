88b6b2d238d63e69d579ace043780bce6a5ee31a
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-18 18:09:21 +0000

    Adding One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -7,5 +7,11 @@ require_relative './exercise_5'
 
 puts "Exercise 6"
 puts "----------"
+puts Employee.count
 
-# Your code goes here ...
+# Adding data to employees
+@store1.employees.create(first_name: "Sameer", last_name: "Menda", hourly_rate: 60)
+@store2.employees.create(first_name: "Madhuri", last_name: "Varudu", hourly_rate: 40)
+@store2.employees.create(first_name: "Bhavamanyu", last_name: "Bora", hourly_rate: 20)
+
+puts Employee.count
